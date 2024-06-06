dc8a52a413f04f5c9876aebd7d0c2ad2fb7c3213
chelle-rr <62896863+chelle-rr@users.noreply.github.com>
2024-06-06 16:21:28 -0400

    Update vid-thumbnails.sh
    Update to process thumbnails in batches of 120, final output to PDF

--- vid-thumbnails.sh
+++ vid-thumbnails.sh
@@ -21,6 +21,10 @@ if [[ -z ${video_list[@]} ]]; then
     exit 1
 fi
 
+# Create a temp directory for thumbnails
+thumbnails_dir="$video_dir/thumbnails"
+mkdir -p –m777 "$thumbnails_dir"
+
 # For each item in the video_list, create a thumbnail. Note: if the video is less than 00:00:02, no thumbnail will be generated
 for video_file in $video_list
 do
@@ -32,7 +36,7 @@ do
     filename_no_ext="${filename%.*}"
 
     # Output thumbnail filename
-    thumbnail_filename="$dir/$filename_no_ext-th.png"
+    thumbnail_filename="$thumbnails_dir/$filename_no_ext-th.png"
 
     # Extract thumbnail with ffmpeg; only reviews first two seconds of file, scales the thumbnail to a max width of 280px
     ffmpeg -ss 00:00:02 -i "$video_file" -hide_banner -loglevel fatal -frames:v 1 -filter:v scale="280:-1" "$thumbnail_filename"
@@ -47,59 +51,38 @@ do
 done
 
 # Find all the thumbnails in the directory
-find "$video_dir" -type f -iname *-th.png > $video_dir/thumb_list.txt
+find "$thumbnails_dir" -type f -iname *-th.png > $thumbnails_dir/thumb_list.txt
 
-# [removed bc unnecessary] Determine number of rows for imagemagick
-# read num_imgs <<< $(sed -n '$=' thumb_list.txt)
-# let num_rows=num_imgs/6
-# let lefto=num_imgs%6
-# if [ $lefto -gt 0 ]
-# then
-#    let num_rows=num_rows+1
-# fi
+# Split thumbnails into groups of 120 (to try to improve montage creation time for large directories? 🤞)
+gsplit -l 120 -a 3 --additional-suffix=.txt $thumbnails_dir/thumb_list.txt $thumbnails_dir/thumb_list_
 
-# Determine number of columns for imagemagick
-read num_imgs <<< $(sed -n '$=' $video_dir/thumb_list.txt)
-if [[ $num_imgs -gt 5 ]]
-then
-   let num_columns=6
-else
-	let num_columns=num_imgs	   
-fi
+# Counter for PNG filenames
+counter=1
 
-# Label the pngs so that the final contact sheet doesn't contain "-th.png" not working though
-# thumb_labels=`find "$video_dir" -type f -iname *-th.png`
-# 
-# for thumb_to_label in $thumb_labels
-# do
-# 	thumb_fn=$(basename -- "$thumb_to_label")
-# 	thumb_fn_no_ext="${thumb_fn%.*}"
-# 	remove="-th"
-# 	thumb_fn_trim=${thumb_fn_no_ext%"$remove"}
-# 	echo $thumb_fn_trim
-# 	
-# done > $video_dir/thumb_labels.txt
+# For each group of thumbnails ...
+for thumb_list_file in "$thumbnails_dir"/thumb_list_*; do
+    # ... create contact sheets
+    montage -label '%t' -font Helvetica -pointsize 10 -size 200x200 @"$thumb_list_file" -geometry 280x190 -tile 6x "$thumbnails_dir/__${filenameinfo}-vid-thumbnails-$counter.png"
 
-# Name the output thumbnails
-# read thumb_filename <<< $(sed -n 's/[A][0-9]\{5\}//' $video_dir)
+    # Check if thumbnails.png was successfully created
+    if test -f "$video_dir/__${filenameinfo}-vid-thumbnails-$counter.png"; then
+        echo "__${filenameinfo}-vid-thumbnails-$counter.png successfully created"
+    else
+        echo "Error in creating __${filenameinfo}-vid-thumbnails-$counter.png"
+    fi
 
-# Create contact sheets
-montage -label '%t' @$video_dir/thumb_list.txt -geometry 280x190 -tile "$num_columns"x $video_dir/_vid-thumbnails.png
-
-# Check if thumbnails.png was successfully created
-if test -f $video_dir/_vid-thumbnails.png; then
-	echo "_vid-thumbnails.png successfully created"
-else
-	echo "Error in creating _vid-thumbnails.png"
-fi
-
-# Remove that no-longer-needed list
-rm $video_dir/thumb_list.txt
-
-# Delete thumbnails
-thumb_list=`find "$video_dir" -type f \( -iname "*-th.png" \)`
-
-for delete_me in $thumb_list
-do
-	rm $delete_me
+    ((counter++))
 done
+
+# Combine thumbnail PNGs into a PDF
+montage "$thumbnails_dir/__${filenameinfo}-vid-thumbnails-"*.png -tile 1x -geometry +0+0 "$video_dir/__${filenameinfo}_vid-thumbnails.pdf"
+
+# Check if PDF was successfully created
+if test -f "$video_dir/__${filenameinfo}_vid-thumbnails.pdf"; then
+    echo "__${filenameinfo}_vid-thumbnails.pdf successfully created"
+else
+    echo "Error creating __${filenameinfo}_vid-thumbnails.pdf"
+fi
+
+# Delete thumbnails directory
+rm -rf "$thumbnails_dir"
